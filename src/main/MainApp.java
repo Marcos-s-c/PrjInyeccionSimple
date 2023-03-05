b28720f3f3f1833e68561b7592a8dc457d7a4f3d
Marcos Saenz <66282209+Marcos-s-c@users.noreply.github.com>
2023-03-04 22:33:21 -0600

    fix

--- src/main/MainApp.java
+++ src/main/MainApp.java
@@ -1,16 +1,16 @@
 package main;
-
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import testbean.MySpringWriterBeanWithDependency;
 
 
+
 public class MainApp {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("META-INF/beans.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("file:METAINF/beans.xml");
         BeanFactory factory = context;
-        testbean.MySpringWriterBeanWithDependency test = (testbean.MySpringWriterBeanWithDependency) factory.getBean("MySpringWriterBeanWithDependency");
+        MySpringWriterBeanWithDependency test = (MySpringWriterBeanWithDependency) factory.getBean("MySpringWriterBeanWithDependency");
         test.run();
     }
 }
